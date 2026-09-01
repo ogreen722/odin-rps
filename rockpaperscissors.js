ac21f4760fc08b49c1a925ec1743cce670393154
Omari <111535653+ogreen722@users.noreply.github.com>
2026-09-01 14:34:52 -0400

    Add human choice to game
    Write code and pseudocode to add the human choice
    This allows the human to choose rock, paper, or
    Scissors and participate in the game.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,6 +13,11 @@ function getComputerChoice(){
             return "Scissors";
     }
 }
+// Write a function that gives the human choice of rock, paper, or scissor.
+// Give a prompt that requires the user to enter a choice and returns the user choice.
+function getHumanChoice(){
+    let choice = prompt("What is your choice?");
+    return choice;
+}
 console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+console.log(getHumanChoice());
